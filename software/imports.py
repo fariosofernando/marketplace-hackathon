1dbebfa8c275157d47dfc675bdcb341b0e51f309
farioso-fernando <farioso.f@gmail.com>
2021-05-03 00:03:09 +0200

    start to code

---
+++ software/imports.py
@@ -0,0 +1,3 @@
+# All import
+
+var = 'init'
---
+++ software/main.py
@@ -0,0 +1,10 @@
+from imports import var
+
+def my_callback(
+    var_paramtre
+):
+    return var_paramtre
+
+if __name__ == '__main__':
+    instanc = my_callback(var)
+    print(instanc)
